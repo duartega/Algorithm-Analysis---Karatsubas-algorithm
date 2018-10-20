3285b6564e6b9f7c5403775d3ec88190df7741f1
gabriel <duartega@sonoma.edu>
2018-10-20 10:28:20 -0700

    Updated code, different format

--- a.py
+++ a.py
@@ -10,89 +10,98 @@ Description:    Code Karatsuba's Algorithm and Exponentiation Algorithm
 """
 
 
-def karatsuba(first_num, second_num):
+def karatsuba(first_num, second_num, d):
     # Create the arrays for storing digits by 1 digit per index
     fn = []
     sn = []
-
-    # Make sure that the numbers are 1 digit
-    if (int(first_num) < 10 and int(second_num) < 10):
+    n = ''
+    n2 = ''
+    if (len(first_num) == 1 and len(second_num) == 1):
         mult = int(first_num) * int(second_num)
         return mult
 
-    # Checks to see if the numbers have the same length for
-    # easier multiplication. Pad with zero on left if not the same length
+
     if (len(first_num) < len(second_num)):
         while (len(first_num) != len(second_num)):
             first_num = '0' + first_num
-    elif (len(first_num) > len(second_num)):
+    if (len(first_num) > len(second_num)):
         while (len(first_num) != len(second_num)):
             second_num = '0' + second_num
 
-    # Set the number of digits to the length of
-    # the string provided
+
     num_of_digits = len(first_num)
 
-    # Append each number as a single digit to
-    # their respective arrays
-    for i in range(num_of_digits):
-        fn.append(int(first_num[i]))
-        sn.append(int(second_num[i]))
+    split = (num_of_digits // 2)
 
-    # Get the number of first half of elements for a1 and b1
-    split = (len(fn)//2)
-
-    # If there are only 3 elements, 3//2 = 1
-    # but you need at least the first 2 in the array
-    if (split % 2 == 1):
+    if (num_of_digits % 2 != 0):
         split += 1
 
-    # Python syntax to get the indexes from
-    # [0:split] and [split:] Ex: split = 2,
-    # [0:2] would get the first two elements
-    if (num_of_digits == 2):
-        a1 = fn[:split]
-        b1 = sn[:split]
-    else:
-        a1 = fn[:split]
-        b1 = sn[:split]
-        a0 = fn[split:]
-        b0 = sn[split:]
+    i = 0
+    j = num_of_digits//2+num_of_digits-1
+    while ( i < num_of_digits):
+        if ( j-1 == i and num_of_digits %2 == 1):
+            first_num = first_num[:i] + '0' + first_num[i:]
+            second_num = second_num[:i] + '0' + second_num[i:]
+            fn.append('0')
+            sn.append('0')
+            i += 1
+            num_of_digits += 1
 
-    # Calculate a two digit number times another two digit number
-    if (num_of_digits == 2):
-        c2 = karatsuba(str(a1[0]),str(b1[0]))
-        c0 = karatsuba(str(a1[1]),str(b1[1]))
-        c1 = karatsuba(str(a1[0] + a1[1]) , str(b1[0] + b1[1])) - (c2 + c0)
-        c  = (int(str(c2) +'00')) + (int(str(c1) +'0')) + c0
-        return c
+        fn.append(first_num[i])
+        sn.append(second_num[i])
+        i += 1
 
-    # Calculate a three digit number times another three digit number Ex: 123 * 123
-    if (num_of_digits == 3):
-        c2 = karatsuba((str(a1[0])+str(a1[1])),(str(b1[0])+str(b1[1]))) # Ex: 12 * 12
-        c0 = a0[0] * b0[0] # We know this will always be a single digit since the length is odd. # Ex: 3 * 3
-        c1 = karatsuba(str(int(str(a1[0]) + str(a1[1]))+ int(str(a0[0]))), str(int(str(b1[0]) + str(b1[1]))+ int(str(b0[0]))) ) - (c2 + c0)
-        c  = (int(str(c2) +'00')) + (int(str(c1) +'0')) + c0
-        return c
+    zp = num_of_digits - split
+    zp1 = zp * 2
 
-    # Calculate a four digit number times another four digit number Ex: 2299 * 4412
-    if (num_of_digits == 4):
-        c2 = karatsuba((str(a1[0])+str(a1[1])),(str(b1[0])+str(b1[1]))) # xx * xx Ex: 22 * 44
-        c0 = karatsuba((str(a0[0])+str(a0[1])),(str(b0[0])+str(b0[1]))) # xx * xx Ex: 99 * 12
-        c1 = karatsuba(str(int(str(a1[0]) + str(a1[1]))+ int(str(a0[0])+str(a0[1]))), str(int(str(b1[0]) + str(b1[1]))+ int(str(b0[0]) + str(b0[1]))) ) - (c2 + c0)
-        c  = int(str(c2) + '0000') + int(str(c1) + '00') + int(c0)
-        return c
+    if (int(first_num) % 2 == 1 and (len(first_num) > 2 or len(second_num) > 2)):
+        zp1 -= 2
+        zp -= 1
+    for i in range(zp1):
+        n+='0'
+    for i in range(zp):
+        n2 += '0'
+    a1 = fn[:split]
+    b1 = sn[:split]
+    a0 = fn[split:]
+    b0 = sn[split:]
+    i = 0
+
+    c2 = int(karatsuba("".join(a1), "".join(b1), d))
+    c0 = karatsuba("".join(a0), "".join(b0), d)
+    c1 = karatsuba(str(int("".join(a1)) + int("".join(a0))),str(int("".join(b1)) + int("".join(b0))), d) - (c2 + c0)
+    p2 = str(c2) + n
+    p1 = str(c1) + n2
+    p0 = c0
+    c  = int((int(str(c2) + n)) + (int(str(c1) + n2)) + int(c0))
+    return c
+
+
+def Exponentiation(a, n):
+    result = a
+
+    for i in range(n - 1):
+        result *= a
+        print(karatsuba(str(result), str(a)))
+        # result *= a
+    return result
+    #    else
+    #       if (n % 2 == 0):
+    #          return
+    """
+    a^n =
+    (a^n/2)^2           if n is even and positive,
+    (a^(n-1)/2)^2 * a   if n is odd,
+    1                   if n = 0.
+    """
+
 
 def main():
     # 5//2 = 2  NOT  3
     x = '0'
 
     # Loop until the user wants to exit
-    while(x != '3'):
-
-        # Grab some input from the user
-        first_num = (input("\nPlease enter a number less than or equal to 1000: "))
-        second_num = (input("Please enter another number less than or equal to 1000: "))
+    while (x != '3'):
 
         # Ask which Task the user would like to run
         print("\n1: Karatsuba's Algorithm")
@@ -103,16 +112,24 @@ def main():
 
         # Run Karatsuba's Algorithm
         if (x == '1'):
-            print("\nThe product of", first_num, "and", second_num, "is:", int(int(first_num)*int(second_num)))
-            c = karatsuba(first_num, second_num)
-            print("Product after karatsuba is: ", c )
+            # Grab some input from the user
+            first_num = str(input("\nPlease enter a number less than or equal to 1000: "))
+            second_num = str(input("Please enter another number less than or equal to 1000: "))
+            print("\nThe product of", first_num, "and", second_num, "is:", int(int(first_num) * int(second_num)))
+            d = len(first_num)
+            c = karatsuba(first_num, second_num, d)
+            print("Product after karatsuba is: ", c)
 
         # Run Exponentiation Algorithm
-        elif(x == '2'):
-            print("Not yet implemented")
-
+        elif (x == '2'):
+            # Grab some input from the user
+            a = int(input("\nPlease enter a number less than or equal to 1000 for the constant: "))
+            n = int(input("Please enter another number less than or equal to 1000 for the power: "))
+            result = Exponentiation(a, n)
+            print("Final Value:", result)
         # Exit
         else:
             exit
+
 
 main()
